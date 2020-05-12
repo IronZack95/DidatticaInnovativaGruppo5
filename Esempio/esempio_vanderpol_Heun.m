bd1a6b900e7b993f0a0859f09f7ef38393acd71d
SamDega <65042421+SamDega@users.noreply.github.com>
2020-05-12 12:18:17 +0200

    Add files via upload

---
+++ Esempio/esempio_vanderpol_Heun.m
@@ -0,0 +1,39 @@
+clc;                                               % Clears the screen
+clear;
+
+h=0.01*pi;        % frazione decimale di pi-->100 passi 
+t = 0:h:pi;                                        
+i = zeros(1,length(t)); 
+y1(1) = 0;     
+y2(1) = 0;% initial condition
+mu = 1;
+% Termini forzanti già presenti nel file del De Falco
+x   = @(t) sin (t) .^ 4;
+xp  = @(t) 4 * sin (t) .^ 3 .* cos (t);
+xpp = @(t) 4 * (3 * sin (t) .^ 2 .* cos (t) .^2 - sin (t) .^ 4);
+mu  = 1;
+f   = @(t) xpp(t) - mu * (1 - x(t).^2) .* xp(t) + x(t);
+% Sistema differenziale
+y1_dot = @(t, y2) y2;
+y2_dot = @(t, y1, y2) mu*(1-y1^2)*y2-y1+f(t);  
+
+for i=1:(length(t)-1)                              % calculation loop
+    k1_1 = y1_dot(t(i), y2(i));
+    k1_2 = y2_dot(t(i), y1(i), y2(i));
+    k2_1 = y1_dot(t(i)+h, y2(i)+k1_2*h);
+    k2_2 = y2_dot(t(i)+h, y1(i)+k1_1*h, y2(i)+k1_2*h);
+
+    y1(i+1) = y1(i) + (1/2)*(k1_1+k2_1)*h;
+    y2(i+1) = y2(i) + (1/2)*(k1_2+k2_2)*h;
+    
+end
+
+plot (t, x(t), t, xp(t), t, y1, 'x', t, y2, 'o')
+% Linea continua soluzione esatta, x e o soluzione numerica
+figure
+plot (t, abs(x(t) - y1))
+title ('errore prima componente')
+
+figure
+plot (t, abs(xp(t) - y2))
+title ('errore seconda componente')
